4ddd07c92305550731a3306371df898e38fe303e
Debela394 <debela394@gmail.com>
2024-04-09 15:15:38 +0300

    The first w3 test commit!

---
+++ w3.py
@@ -0,0 +1,7 @@
+print("The 1st w3 test code")
+print("Hello w3")
+print(' ')
+
+print("Again this is test one code")
+print("Hello this test one code")
+print('')
---
+++ w31.py
@@ -0,0 +1,3 @@
+print("Second test code of w3")
+print("HEllo this is 2nd test code of w3")
+print('')
