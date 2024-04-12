3a9eae35ee14d9462248c2a762a711439c014597
Debela Tesfaye <96177040+Debela394@users.noreply.github.com>
2024-04-12 10:54:54 +0300

    Update w3.py space editing

--- w3.py
+++ w3.py
@@ -1,20 +1,12 @@
 print("The 1st w3 test code")
 print("Hello w3")
 print(' ')
-
 print("Again this is test one code")
-
 print('')
-
 print("Branch Test")
-
 print("Again this is test one code")
-
 print(' ')
-
 print("For emergency-fix on w3")
-
 print('')
-
 print("For github push")
-print('')
+
